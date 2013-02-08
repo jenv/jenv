50e3ac1c739d60c1fd84ed47255cf452e36529f1
Frank Pavageau <frank.pavageau@gmail.com>
2013-02-08 23:28:49 +0100

    Don't override a globally set MAVEN_OPTS, especially when there are no jenv options. Besides, when trying to set exportedKey[MAVEN_OPTS] as MAVEN_OPTS is already set, MAVEN_OPTS as the key is expanded, resulting in an error message: -Xmx1g -XX:MaxPermSize=256m: syntax error in expression (error token is ":MaxPermSize=256m")

--- plugins/maven/etc/jenv.d/exec/maven-before.bash
+++ plugins/maven/etc/jenv.d/exec/maven-before.bash
@@ -1,4 +1,6 @@
 if [ "$1" = "mvn" ]; then            
-  exportVariable MAVEN_OPTS $JENV_OPTIONS
-  unset JENV_OPTIONS        
+  if [ -n "$JENV_OPTIONS" -a -z "$MAVEN_OPTS" ]; then
+    exportVariable MAVEN_OPTS $JENV_OPTIONS
+    unset JENV_OPTIONS        
+  fi
 fi
