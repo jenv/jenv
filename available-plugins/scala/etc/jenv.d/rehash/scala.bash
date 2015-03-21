05805c0dc863134761a9235a22280923607d44b8
Josh Hagins <hagins.josh@gmail.com>
2015-03-21 02:27:36 -0400

    Add shim for fsc (fast Scala compiler) to scala plugin

--- available-plugins/scala/etc/jenv.d/rehash/scala.bash
+++ available-plugins/scala/etc/jenv.d/rehash/scala.bash
@@ -5,12 +5,14 @@ backuppath=$PATH
 PATH="$(remove_from_path "${JENV_ROOT}/shims")"
 SCALA_BIN="$(command -v "scala" || true)"
 SCALAC_BIN="$(command -v "scalac" || true)"
+FSC_BIN="$(command -v "fsc" || true)"
 
 
 PATH=$backuppath
 
 make_shims "$SCALA_BIN"    
 make_shims "$SCALAC_BIN"  
+make_shims "$FSC_BIN"
 
 
 
