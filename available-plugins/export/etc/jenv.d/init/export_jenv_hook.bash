3f5e64e856493de252208e7e45edd2fbcf099069
Josh Hagins <hagins.josh@gmail.com>
2015-04-03 10:48:45 -0400

    Run _jenv_export_hook after existing PROMPT_COMMAND
    Prepending the hook to PROMPT_COMMAND messes up return code reporting.

--- available-plugins/export/etc/jenv.d/init/export_jenv_hook.bash
+++ available-plugins/export/etc/jenv.d/init/export_jenv_hook.bash
@@ -7,7 +7,7 @@
    }
 
 if ! [[ "$PROMPT_COMMAND" =~ _jenv_export_hook ]]; then
-    PROMPT_COMMAND="_jenv_export_hook;$PROMPT_COMMAND";
+    PROMPT_COMMAND="$PROMPT_COMMAND;_jenv_export_hook";
 fi
 
 
