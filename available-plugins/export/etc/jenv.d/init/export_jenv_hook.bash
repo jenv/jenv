e50244e193c68c98569fa26323e98b47b09fc62f
Pablo Díaz-López <padilo@gmail.com>
2016-05-31 18:06:34 +0200

    Propagating error_code on jenv hook

--- available-plugins/export/etc/jenv.d/init/export_jenv_hook.bash
+++ available-plugins/export/etc/jenv.d/init/export_jenv_hook.bash
@@ -1,16 +1,18 @@
 #export
 
 
- _jenv_export_hook() {
+_jenv_export_hook() {
+  local error_code=$?
   export JAVA_HOME=$(jenv javahome)
   export JENV_FORCEJAVAHOME=true
 
   if [ -e "$JAVA_HOME/bin/javac" ]
   then
-    export JDK_HOME=$(jenv javahome)
+    export JDK_HOME=$JAVA_HOME
     export JENV_FORCEJDKHOME=true
   fi
-   }
+  $(exit $error_code)
+}
 
 if ! [[ "$PROMPT_COMMAND" =~ _jenv_export_hook ]]; then
     PROMPT_COMMAND="_jenv_export_hook;$PROMPT_COMMAND";
