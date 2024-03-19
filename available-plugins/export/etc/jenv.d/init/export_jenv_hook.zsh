d53457e2f27451521bb6bd72617fe57622ebc7ce
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2024-03-19 02:53:28 +0000

    feat: handle system version better
    Use java from the path with jenv shims and locations removed.
    Set JAVA_HOME to be the linked java home from above, if it exists,
    otherwise search for OS utility programs which can pinpoint the javahome
    like libexec java_home or alternatives.
    Fallback to whatever JAVA_HOME is currently set to if it can't otherwise
    be resolved.

--- available-plugins/export/etc/jenv.d/init/export_jenv_hook.zsh
+++ available-plugins/export/etc/jenv.d/init/export_jenv_hook.zsh
@@ -1,7 +1,20 @@
 #export
 
- _jenv_export_hook() {
-  export JAVA_HOME=$(jenv javahome)
+_jenv_export_hook() {
+  if ! type -w jenv &>/dev/null
+  then
+    return
+  fi
+  export JENV_FORCEJAVAHOME=
+  export JENV_FORCEJDKHOME=
+
+  local home
+  if ! home=$(jenv javahome 2>/dev/null)
+  then
+    return
+  fi
+
+  export JAVA_HOME=$home
   export JENV_FORCEJAVAHOME=true
 
   if [ -e "$JAVA_HOME/bin/javac" ]
@@ -9,14 +22,14 @@
     export JDK_HOME="$JAVA_HOME"
     export JENV_FORCEJDKHOME=true
   fi
- }
+}
 
 #echo "configure export plugin for ZSH"
 function install_hook {
   emulate -LR zsh
   typeset -ag precmd_functions
   if [[ -z $precmd_functions[(r)_jenv_export_hook] ]]; then
-        precmd_functions+=_jenv_export_hook;
+    precmd_functions+=_jenv_export_hook;
   fi
 }
 install_hook
