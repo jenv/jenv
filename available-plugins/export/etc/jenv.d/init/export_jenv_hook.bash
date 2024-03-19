47ca7f22b67d7620e6db33ab061bec2eeb857976
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2024-03-19 02:53:28 +0000

    feat: #422 handle system version better
    Use the current env JAVA_HOME when using the system version, or use the
    JAVA_HOME from when it was last set.

--- available-plugins/export/etc/jenv.d/init/export_jenv_hook.bash
+++ available-plugins/export/etc/jenv.d/init/export_jenv_hook.bash
@@ -1,8 +1,32 @@
 #export
 
+_jenv_export_hook() {
+  export JENV_FORCEJAVAHOME=
+  export JENV_FORCEJDKHOME=
+  export __JENV_CURRENT_VERSION
+  export __JENV_LAST_VERSION=$__JENV_CURRENT_VERSION
+  export __JENV_LAST_SYSTEM_JAVA_HOME
 
- _jenv_export_hook() {
-  export JAVA_HOME=$(jenv javahome)
+  local home
+  if ! read -r __JENV_CURRENT_VERSION home < <(jenv javahome --detail 2>/dev/null)
+  then
+    return
+  fi
+
+  if [ "$__JENV_CURRENT_VERSION" = "system" ]
+  then
+    if [ -z "$__JENV_LAST_VERSION" ]
+    then
+      continue
+    elif [ "$__JENV_LAST_VERSION" != "system" ]
+    then
+      home=$__JENV_LAST_SYSTEM_JAVA_HOME
+    else
+      __JENV_LAST_SYSTEM_JAVA_HOME=$home
+    fi
+  fi
+
+  export JAVA_HOME=$home
   export JENV_FORCEJAVAHOME=true
 
   if [ -e "$JAVA_HOME/bin/javac" ]
@@ -10,11 +34,10 @@
     export JDK_HOME="$JAVA_HOME"
     export JENV_FORCEJDKHOME=true
   fi
-   }
+}
 
 if ! [[ "$PROMPT_COMMAND" =~ _jenv_export_hook ]]; then
-    PROMPT_COMMAND="_jenv_export_hook;$PROMPT_COMMAND";
+  PROMPT_COMMAND="_jenv_export_hook;$PROMPT_COMMAND";
 fi
-
 
 _jenv_export_hook
