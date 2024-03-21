4b0068a97cfc61d151b1a8ce9cc190c01f91953d
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2024-03-21 22:30:39 +0000

    fix: bug in export variables appending unnecessary args and spaces

--- available-plugins/ant/etc/jenv.d/exec/ant-before.bash
+++ available-plugins/ant/etc/jenv.d/exec/ant-before.bash
@@ -1,10 +1,10 @@
-if [ "$1" = "ant" ]; then            
+if [ "$1" = "ant" ]; then
   if [ -n "$JENV_OPTIONS" ]; then
     if [ -z "$ANT_OPTS" ]; then
-      exportVariable ANT_OPTS $JENV_OPTIONS
+      exportVariable ANT_OPTS "$JENV_OPTIONS"
     else
       echo "ANT_OPTS is set and not overridden by jenv"
     fi
-    unset JENV_OPTIONS        
+    unset JENV_OPTIONS
   fi
 fi
--- available-plugins/maven/etc/jenv.d/exec/maven-before.bash
+++ available-plugins/maven/etc/jenv.d/exec/maven-before.bash
@@ -1,10 +1,10 @@
-if [ "$1" = "mvn" -o "$1" = "mvnDebug"  ]; then            
+if [ "$1" = "mvn" -o "$1" = "mvnDebug"  ]; then
   if [ -n "$JENV_OPTIONS" ]; then
     if [ -z "$MAVEN_OPTS" ]; then
-      exportVariable MAVEN_OPTS $JENV_OPTIONS
+      exportVariable MAVEN_OPTS "$JENV_OPTIONS"
     else
       echo "MAVEN_OPTS is set and not overridden by jenv"
     fi
-    unset JENV_OPTIONS        
+    unset JENV_OPTIONS
   fi
 fi
